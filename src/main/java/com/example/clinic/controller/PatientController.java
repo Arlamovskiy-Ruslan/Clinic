7bf62e47deb3f8037f415f8f6437c8938ac4b6be
Arlamovskiy-Ruslan <55766007+Arlamovskiy-Ruslan@users.noreply.github.com>
2021-06-04 20:31:09 +0300

    controller

--- src/main/java/com/example/clinic/controller/PatientController.java
+++ src/main/java/com/example/clinic/controller/PatientController.java
@@ -1,9 +1,52 @@
 package com.example.clinic.controller;
 
+import com.example.clinic.models.Patient;
+import com.example.clinic.repo.PatientRepo;
+import com.example.clinic.service.PatientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/")
 public class PatientController {
 
+    private final PatientRepo patientRepo;
+
+    private final PatientService patientService;
+
+    @Autowired
+    public PatientController(PatientRepo patientRepo, PatientService patientService) {
+        this.patientRepo = patientRepo;
+        this.patientService = patientService;
+    }
+
+    @RequestMapping("/")
+    public List<Patient> getAllPatients() {
+        return patientRepo.findAll();
+    }
+
+    @RequestMapping("/patient/{id}")
+    public Patient retrievePatient(@PathVariable long id) {
+        Optional<Patient> patients = patientRepo.findById(id);
+        return patients.get();
+    }
+
+    @RequestMapping(value = "/patient/{id}/delete", method = RequestMethod.DELETE)
+    public void deletePatient(@PathVariable long id) {
+        patientRepo.deleteById(id);
+    }
+
+    @RequestMapping(value = "/patient/create", method = RequestMethod.POST)
+    public void createPatient(@RequestBody Patient patient) {
+        patientService.createPatient(patient);
+    }
+
+    @RequestMapping(value = "/patient/create",method = RequestMethod.PUT)
+    public void updatePatient(@RequestBody Patient patient, @PathVariable long id) {
+        patientService.updatePatient(patient, id);
+    }
 }
+
