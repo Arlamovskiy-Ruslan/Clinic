14bd9ad74efe04c70f32b706ee266cc45b719ca1
Arlamovskiy-Ruslan <55766007+Arlamovskiy-Ruslan@users.noreply.github.com>
2021-06-04 20:30:47 +0300

    repositore

--- src/main/java/com/example/clinic/repo/PatientRepo.java
+++ src/main/java/com/example/clinic/repo/PatientRepo.java
@@ -1,11 +1,11 @@
 package com.example.clinic.repo;
 
-import com.example.clinic.models.Patients;
+import com.example.clinic.models.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PatientRepo extends JpaRepository<Patients, Long> {
+public interface PatientRepo extends JpaRepository<Patient, Long> {
 
 
 }
