1dd2fc426fd7f62459d8c3d55802894665c22ffa
Arlamovskiy-Ruslan <55766007+Arlamovskiy-Ruslan@users.noreply.github.com>
2021-06-04 20:30:15 +0300

    service

--- src/main/java/com/example/clinic/service/PatientService.java
+++ src/main/java/com/example/clinic/service/PatientService.java
@@ -1,7 +1,44 @@
 package com.example.clinic.service;
 
+import com.example.clinic.models.Patient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import com.example.clinic.repo.PatientRepo;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.Optional;
 
 @Service
 public class PatientService {
+
+    @Autowired
+    private PatientRepo patientRepo;
+
+
+    public ResponseEntity<Object> createPatient(@RequestBody Patient patient) {
+        Patient savedPatients = patientRepo.save(patient);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(savedPatients.getId()).toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+
+    public ResponseEntity<Object> updatePatient(@RequestBody Patient patient, @PathVariable long id) {
+
+        Optional<Patient> patientOptional = patientRepo.findById(id);
+
+        if (!patientOptional.isPresent())
+            return ResponseEntity.notFound().build();
+
+        patient.setId(id);
+
+        patientRepo.save(patient);
+
+        return ResponseEntity.noContent().build();
+    }
 }
