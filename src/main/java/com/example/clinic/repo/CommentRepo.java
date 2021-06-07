96153515e840a6407798ac1f83de058508c8072a
Arlamovskiy-Ruslan <55766007+Arlamovskiy-Ruslan@users.noreply.github.com>
2021-06-07 19:58:33 +0300

    Repository

--- src/main/java/com/example/clinic/repo/CommentRepo.java
+++ src/main/java/com/example/clinic/repo/CommentRepo.java
@@ -1,14 +1,11 @@
 package com.example.clinic.repo;
 
-import com.example.clinic.models.Comment;
+import com.example.clinic.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository
-public interface CommentRepo extends JpaRepository<Comment, Long> {
-
-    List<Comment> findByUserFirstName(String firstName);
+public interface CommentRepo extends JpaRepository<Comment,Long> {
 
 }
--- src/main/java/com/example/clinic/repo/PatientRepo.java
+++ src/main/java/com/example/clinic/repo/PatientRepo.java
@@ -1,6 +1,6 @@
 package com.example.clinic.repo;
 
-import com.example.clinic.models.Patient;
+import com.example.clinic.entity.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
