76b8f0602c2b3dae8d66dc267e7636f6e0a8d82a
Arlamovskiy-Ruslan <55766007+Arlamovskiy-Ruslan@users.noreply.github.com>
2021-06-09 20:58:01 +0300

    React.js

--- src/main/webapp/reactjs/src/components/PatientComponent.js
+++ src/main/webapp/reactjs/src/components/PatientComponent.js
@@ -1,41 +1,52 @@
 import React from "react";
 import axios from "axios";
+import {Button, Card} from "react-bootstrap";
 
 class PatientComponent extends React.Component {
 
-   constructor(props) {
-       super(props);
-       this.state = {
-           patients:[]
-       }
-   }
+    constructor(props) {
+        super(props);
+        this.state = {
+            patients: []
+        }
+    }
 
-   componentDidMount() {
-       axios.get("http://localhost:8080")
-           .then(res => res.data)
-           .then((data) => {
-               this.setState({patients: data});
-           });
-   }
+    componentDidMount() {
+       this.findAllPatients();
+    }
+
+    findAllPatients(){
+        axios.get("http://localhost:8080")
+            .then(res => res.data)
+            .then((data) => {
+                this.setState({patients: data});
+            });
+    }
 
     render() {
         return (
-            <div>
-                {
-                    this.state.patients.map(
-                        patient =>
-                            <div key={patient.id} className="Patients">
-                                <hr className="hr1"/>
+            <Card>
+                <div className="Search">
+                    <input className="search_user" type="text" size="15"/>
+                    <Button className="add_button">New Patient</Button>
+                </div>
+                <div className="Patients">
+                    {
+                        this.state.patients.map(
+                            patient =>
+                                <div key={patient.id}>
+                                    <hr className="hr1"/>
 
-                                <a>{patient.firstName}</a>
-                                <a>{patient.lastName}</a>
-                                <p>{patient.dateOfBirth}</p>
+                                    <a>{patient.firstName}</a>
+                                    <a>{patient.lastName}</a>
+                                    <p>{patient.dateOfBirth}</p>
 
-                            </div>
-                    )
-                }
-            </div>
-            
+                                </div>
+                        )
+                    }
+                </div>
+                <div className="vl"></div>
+            </Card>
         )
     }
 
