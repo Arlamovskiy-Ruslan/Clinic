ebbefab4d3e9b624cd44f635bee4ddd3678ae092
Arlamovskiy-Ruslan <55766007+Arlamovskiy-Ruslan@users.noreply.github.com>
2021-07-15 23:13:20 +0300

    .

--- src/main/java/com/example/clinic/ClinicApplication.java
+++ src/main/java/com/example/clinic/ClinicApplication.java
@@ -7,4 +7,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ClinicApplication {
     public static void main(String[] args) {
         SpringApplication.run(ClinicApplication.class, args);
-    }}
+    }
+}
